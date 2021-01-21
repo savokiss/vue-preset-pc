ed27b6e78ed3d295f6d42d1352fcf1b9a750a40c
jasehuang <jasehuang@tencent.com>
2021-01-21 19:39:32 +0800

    fix(vue.config): sass option upgrade
    fix #5

--- template/vue.config.js
+++ template/vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `
+        prependData: `
         @import "@/styles/_variables.scss";
         @import "@/styles/_mixins.scss";
         @import "@/styles/_functions.scss";
